3c72064142621874912bba6dec1454f2f4a6eee7
Arjun Sharma <119435054+arjun54244@users.noreply.github.com>
2025-05-27 17:05:21 +0530

    Update HeroSection.tsx
    change the title of hero section

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
         <div className='min-h-[60vh] flex flex-col-reverse gap-14 mt-5 lg:gap-0 lg:flex-row items-center justify-between'>
             <div className="space-y-10 text-center lg:text-left">
                 <h1 className='text-4xl lg:text-7xl font-bold'>
-                    Nice to meet you! 👋 <br />
+                    Nice to meet you! 👋❤ <br />
                     <span className='underline underline-offset-8 decoration-green-500'>{"I'm Arjun Sharma."}</span>
                 </h1>
                 <p className='md:w-96 text-lg text-gray-300'>
